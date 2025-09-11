d1d1dd2f967e4eaaca14b813f5aebfe7ea5cfcf7
Nirmal Singh <nirmalsinghh007@gmail.com>
2025-09-11 09:39:13 +0530

    local storage add and other functionality

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-import { tweetData } from "/data.js"
+import { tweetData as data } from "/data.js"
+
+let tweetData = JSON.parse(localStorage.getItem('tweetData')) || data
 
 // variable 
 const tweetsAndCommentsContainer = document.getElementById("tweets-and-comments-container")
@@ -6,6 +8,8 @@ const tweetsContainer = document.getElementById('tweets-container')
 const commentsContainer = document.getElementById("comments-container")
 const tweetInput = document.getElementById("tweet-input")
 const tweetBtn = document.getElementById('tweet-btn')
+
+console.log(JSON.parse(localStorage.getItem("comment-section")))
 
 // handle document events
 document.addEventListener('click', (e)=> {
@@ -18,12 +22,19 @@ document.addEventListener('click', (e)=> {
     e.target.dataset.trash && handleTrashClick(e.target)
 })
 
+// save date to local storage
+function saveToLocalStorage(){
+    localStorage.setItem("tweetData", JSON.stringify(tweetData))
+}
+
 // Get twees data
 function getTweetsData(){
 
     let tweets = ``
     tweetData.forEach( tweet => {
         const {handle, isLiked, isRetweeted, isUser, likes, retweets, profilePic, tweetText, replied, uuid} = tweet
+
+        const isCommentHidden = localStorage.getItem(`comment-section-${uuid}`) === 'true'
 
         tweets += `
             <div class="tweet-container">
@@ -40,37 +51,41 @@ function getTweetsData(){
                         ${isUser ? `<span><i class="fa-solid fa-trash" data-trash=${uuid}></i></span>` : ""}
                     </div>
                 </div>
-                <div class="comment">
-                    <img src="./images/captainamerica.jpeg" class="profile-pic">
-                    <input type="text" name="comment" placeholder="Write response..." autocomplete="off" class="comment-input">
-                    <span id="comment-post-btn"><i class="fa-regular fa-paper-plane" data-post-comment=${uuid}></i></span>
+                <div id="comment-section-${uuid}" class="comment-section ${isCommentHidden ? "hide" : ""}">
+                    ${getComment(uuid)}
+                    ${replied.length > 0 ? replied.map(getReply).join("") : ""}
                 </div>
             </div>
         `
-
-        if(tweet.replied.length > 0){
-            tweet.replied.forEach( reply => {
-                const {handle, isCommented, profilePic, tweetText, likes, uuid, isLiked} = reply
-
-                tweets += `
-                <div class="comment-container">
-                    <div class="tweet-user-info">
-                        <img src="./images/${profilePic}" alt="Image of${handle}" class="profile-pic">
-                        <p class="handle-p">${handle}</p>
-                    </div>
-                    <div class="tweet-feed">
-                        <p class="tweet">${tweetText}</p>
-                        <div class="tweet-feedback">
-                            <span><i class="fa-solid fa-heart ${isLiked && "liked"}" data-comment-like=${uuid}></i>${likes}</span>     
-                            ${isCommented ? `<span><i class="fa-solid fa-trash" data-comment-trash=${uuid}></i></span>` : ""}
-                        </div>
-                    </div>
-                </div>`
-            })
-        }
     })
-
     return tweets
+}
+
+function getComment(uuid){
+    return `<div class="comment">
+        <img src="./images/captainamerica.jpeg" class="profile-pic">
+        <input type="text" name="comment" placeholder="Write response..." autocomplete="off" class="comment-input">
+        <span id="comment-post-btn"><i class="fa-regular fa-paper-plane" data-post-comment=${uuid}></i></span>
+    </div>`
+}
+
+function getReply(reply){
+    const {handle, isCommented, profilePic, tweetText, likes, uuid, isLiked} = reply
+
+    return `
+    <div class="comment-container">
+        <div class="tweet-user-info">
+            <img src="./images/${profilePic}" alt="Image of${handle}" class="profile-pic">
+            <p class="handle-p">${handle}</p>
+        </div>
+        <div class="tweet-feed">
+            <p class="tweet">${tweetText}</p>
+            <div class="tweet-feedback">
+                <span><i class="fa-solid fa-heart ${isLiked && "liked"}" data-comment-like=${uuid}></i>${likes}</span>     
+                ${isCommented ? `<span><i class="fa-solid fa-trash" data-comment-trash=${uuid}></i></span>` : ""}
+            </div>
+        </div>
+    </div>`    
 }
 
 getTweetsData()
@@ -78,6 +93,7 @@ getTweetsData()
 function renderTweet(){
     tweetsContainer.innerHTML = getTweetsData()
 }
+
 renderTweet()
 
 const handleLikeClick = function(id){ 
@@ -92,6 +108,8 @@ const handleLikeClick = function(id){
         likeEl.likes++
         likeEl.isLiked = true
     }
+
+    saveToLocalStorage()
     renderTweet()
 }
 
@@ -109,21 +127,20 @@ const handleRetweetClick = function(id){
         retweetEl.isRetweeted = true
     }
 
+    saveToLocalStorage()
     renderTweet()
-
 }
 
 const handleCommentClick = function(id){
     tweetData.forEach( comment => {
         if(comment.uuid === id.dataset.comment){
-            const comments = Array.from(id.closest("#tweets-container").querySelector(".comment-container"))
-            comments.forEach(comment => comment.classList.toggle("hide"))
+            document.getElementById(`comment-section-${comment.uuid}`).classList.toggle("hide")   
+            localStorage.setItem(`comment-section-${id.dataset.comment}`, JSON.stringify(document.getElementById(`comment-section-${id.dataset.comment}`).classList.contains("hide")))
         }
     })
 }
 
 const handleCommentLikeClick = function(id){ 
-
     tweetData.forEach(tweet => {
         tweet.replied.forEach(item => {
             if(item.uuid === id.dataset.commentLike){
@@ -137,19 +154,24 @@ const handleCommentLikeClick = function(id){
             }
         })
     })
+
+    saveToLocalStorage()
     renderTweet()
 }
 
 const handlePostCommentClick = function(id){
+    // ensuring clicked element uuid
     const clickedEl = tweetData.filter(tweet => {
         return tweet.uuid === id.dataset.postComment
     })[0]
 
+    // ensuring input is empty
     const commentInputs = document.getElementsByClassName("comment-input")
     const input = Array.from(commentInputs).filter( input => {
         return input.value !== ""
     })[0]
 
+    // adding comment to the comments array
     clickedEl.replied.unshift({
         handle: "@captainamerica",
         profilePic: "captainamerica.jpeg",
@@ -161,8 +183,9 @@ const handlePostCommentClick = function(id){
         uuid: crypto.randomUUID()
     })
 
+    saveToLocalStorage()
+    // re-rendering the DOM
     renderTweet()
-    // console.log(clickedEl.replied)
 }
 
 const handleCommentTrash = function(id){
@@ -170,6 +193,8 @@ const handleCommentTrash = function(id){
         tweet.replied.forEach( item => {
             if(item.uuid === id.dataset.commentTrash){
                 tweet.replied.shift()
+
+                saveToLocalStorage()
                 renderTweet()
             }
         })
@@ -178,12 +203,14 @@ const handleCommentTrash = function(id){
 }
 
 const handleTrashClick = function(id){
-    tweetData.forEach(tweet => {
-        if(tweet.uuid === id.dataset.trash){
-            id.closest(".tweet-container").style.display = "none"
-        }
+    let index = tweetData.findIndex( tweet => tweet.uuid === id.dataset.trash)
 
-    })
+    // removing the tweet from the data
+    if(index > -1){
+        tweetData.splice(index, 1)
+        saveToLocalStorage()
+        renderTweet()
+    }
 }
 
 tweetBtn.addEventListener('click', ()=> {
@@ -204,6 +231,8 @@ tweetBtn.addEventListener('click', ()=> {
             isUser: true,
             uuid: crypto.randomUUID()
         })
+
+        saveToLocalStorage()
         renderTweet()  
         tweetInput.value = ""
     }
